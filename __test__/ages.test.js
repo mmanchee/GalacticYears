729bdb7b1ad5eab04002a1fffca5b02e6ba3ba9d
Mike Manchee <mikemanchee@gmail.com>
2020-09-04 11:33:10 -0700

    add function to test 4 for addDaysAlive function - Passed

--- __test__/ages.test.js
+++ __test__/ages.test.js
@@ -12,15 +12,16 @@ describe('addUserData', () => {
 
   test('Should correctly add the userData object to ages object', () => {
     const ages = new Ages();
-    ages.addUserData({name: "Mike", dob: "Sept 21 1977", expAge: 81});
-    expect(ages.user).toEqual({name: "Mike", dob: "Sept 21 1977", expAge: 81});
+    ages.addUserData({name: "Mike", dob: "09/21/1977", expAge: 81});
+    expect(ages.user).toEqual({name: "Mike", dob: "09/21/1977", expAge: 81});
   })
 })
 describe('addDaysAlive', () => {
 
   test('Should correctly calculate the days alive and add to ages object', () => {
     const ages = new Ages();
-    ages.addDaysAlive({dob: "Sept 21 1977"});
+    ages.user = {name: "Mike", dob: "09/21/1977", expAge: 81};
+    ages.addDaysAlive();
     expect(ages.daysAlive).toEqual(15689); // test will fail every other day except Sept 4th 2020, must add days from that date too pass
   })
 })
--- src/js/ages.js
+++ src/js/ages.js
@@ -6,5 +6,21 @@ export class Ages {
 
   addUserData(user) {
     this.user = user;
+    this.addDaysAlive();
+  }
+
+  addDaysAlive() {
+    let date = new Date();
+    let month = String(date.getMonth() + 1).padStart(2, '0');
+    let year = date.getFullYear();
+    let day = String(date.getDate()).padStart(2, '0');
+    let tDate = month + "/" + day + "/" + year;
+    let today = new Date(tDate);
+    let ddob = new Date(this.user.dob);
+
+    let timeDiff = today.getTime() - ddob.getTime();
+    let dayDiff = timeDiff / (1000 * 3600 * 24);
+
+    this.daysAlive = dayDiff;
   }
 }
